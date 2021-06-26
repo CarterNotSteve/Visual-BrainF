bb0d3339faac164c05da3abd79ffaa3844782ca1
BuildTools <unconfigured@null.spigotmc.org>
2021-06-26 15:12:21 -0500

    Initial commit

---
+++ Visual_brainf.py
@@ -0,0 +1,134 @@
+import sys, getopt
+
+global inputfile
+inputfile = ""
+
+l, p = getopt.getopt(sys.argv[1:], "i:", ["input="])
+for o, a in l:
+    if o in ["i", "--input"]:
+        inputfile = a
+
+if inputfile == "":
+    print("No input file found! (please pass the --input parameter)")
+    sys.exit(1)
+
+x = open(inputfile, "r")
+y = x.readlines()
+
+tape = [0]
+global tape_head
+tape_head = 0
+looping = False
+start_pos = []
+end_pos = []
+instructions_pointer = 0
+com_num = 0
+ln_num = 0
+
+# remove newline characters from lists, so that they aren't parsed. ####################################################
+
+for i in y:
+    c = i
+    i = list(i)
+    if i[len(i) - 1] == "\n":
+        i.remove("\n")
+    i = "".join(i)
+    y[y.index(c)] = i
+    com_num += 1
+
+strins = " ".join(y)
+# print(strins, instructions_pointer, "\n")
+
+
+# time to parse the brain f ############################################################################################
+
+def cmd_add():
+    tape[tape_head] += 1
+    if tape[tape_head] > 255:
+        tape[tape_head] = 0
+
+
+def cmd_sub():
+    tape[tape_head] -= 1
+    if tape[tape_head] <= -1:
+        tape[tape_head] = 255
+
+
+def cmd_right():
+    global tape_head
+    tape_head += 1
+    if tape_head >= len(tape):
+        tape.append(0)
+
+
+def cmd_left():
+    global tape_head
+    tape_head -= 1
+    if tape_head < 0:
+        tape.insert(0, 0)
+        tape_head = 0
+
+
+def cmd_if_not_zero():
+    global tape_head
+    global start_pos
+    global instructions_pointer
+    if tape[tape_head] != 0:
+        start_pos.append(instructions_pointer)
+
+    else:
+        instructions_pointer = end_pos.pop()
+
+
+def cmd_end_loop():
+    global tape_head
+    global start_pos
+    global instructions_pointer
+    end_pos.append(instructions_pointer)
+    instructions_pointer = start_pos.pop() - 1
+
+
+def cmd_in():
+    global tape_head
+    tmp = sys.stdin.read(1)
+    tape[tape_head] = ord(tmp)
+
+
+def cmd_print():
+    print(chr(tape[tape_head]), end="")
+
+
+def cmd_print_val():
+    print(str(tape[tape_head]), end="")
+
+
+funcs = {
+    "+": cmd_add,
+    "-": cmd_sub,
+    ">": cmd_right,
+    "<": cmd_left,
+    "[": cmd_if_not_zero,
+    "]": cmd_end_loop,
+    ".": cmd_print,
+    ",": cmd_in,
+    "^": cmd_print_val
+}
+
+
+while instructions_pointer < len(strins):
+    cmd = strins[instructions_pointer]
+    # print("%s:%d" % (cmd, instructions_pointer))
+
+    if cmd in funcs.keys():
+        funcs[cmd]()
+
+    # print(tape, tape_head, cmd)
+    # print(start_pos)
+
+    instructions_pointer += 1
+
+# debug output #########################################################################################################
+"""
+for e in y:
+    print(e)
+"""
